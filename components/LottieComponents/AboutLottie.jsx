e947b9e96c02f385f64ebba9e5cf35e6be3b2959
MOHAMMED NABIL AL-AHMADI <58184415+Mhmd-alahmadi@users.noreply.github.com>
2024-05-02 18:12:42 +0300

    edit

--- components/LottieComponents/AboutLottie.jsx
+++ components/LottieComponents/AboutLottie.jsx
@@ -1,12 +1,12 @@
 import Lottie from "lottie-react";
 import React from "react";
-import {Smart} from "@/public/Lottie/about.json";
+import Smart2 from "@/public/Lottie/About.json";
 const About = () => {
     return (
       <>
        <Lottie
        color="#000"
-        animationData={Smart}
+        animationData={Smart2}
         
         loop={true}
       />
