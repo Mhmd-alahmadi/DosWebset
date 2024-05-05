6929e0f2430f3a9421bded0f50e55b402367cbf1
MOHAMMED NABIL AL-AHMADI <58184415+Mhmd-alahmadi@users.noreply.github.com>
2024-05-05 20:39:02 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
   }, []);
   return (
     <html lang="en">
+      <title>محفظة جيب الالكترونية/title>
       <SpeedInsights/>
       <body className={`${myFont.variable} bg-jabiBg`  } >
 
